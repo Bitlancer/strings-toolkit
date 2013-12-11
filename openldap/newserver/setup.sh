cbef948c5a526c0352fcfb5d373307f1436e03e9
mjuszczak <mjuszczak@skua.dfw01.strings-infra.net>
2013-12-11 22:32:26 +0000

    updating various tools

--- openldap/newserver/setup.sh
+++ openldap/newserver/setup.sh
@@ -2,30 +2,35 @@
 
 # Sanity checks
 if [ ! -f modified.ldif ]; then
-    echo "Missing modified.ldif";
+    echo "Missing modified.ldif" 1>&2
     exit 1
 fi
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 if [ -d /var/lib/ldap-strings ]; then
-    echo "Woh there cowboy!  You could blow this whole thing away.... /var/lib/ldap-strings exists!"
+    echo "Woh there cowboy!  You could blow this whole thing away.... /var/lib/ldap-strings exists!" 1>&2
     exit 1
 fi
 
 # First, make sure OpenLDAP isn't running
-sudo /sbin/service slapd stop
+/sbin/service slapd stop
 
 # Remove some directory contents
-sudo rm -rf /etc/openldap/slapd.d/*
-sudo rm -rf /var/lib/ldap/*
+rm -rf /etc/openldap/slapd.d/*
+rm -rf /var/lib/ldap/*
 
 # Add in modified.ldif
-sudo slapadd -n 0 -F /etc/openldap/slapd.d -l modified.ldif 
+slapadd -n 0 -F /etc/openldap/slapd.d/ -l modified.ldif 
 chown -R ldap:ldap /etc/openldap/slapd.d /var/lib/ldap
 
 # Create /var/lib/ldap-strings
-sudo mkdir /var/lib/ldap-strings
-sudo chown ldap:ldap /var/lib/ldap-strings 
-sudo chmod 700 /var/lib/ldap-strings
+mkdir /var/lib/ldap-strings
+chown ldap:ldap /var/lib/ldap-strings 
+chmod 700 /var/lib/ldap-strings
 
 # Start slapd
-sudo /sbin/service slapd start
+/sbin/service slapd start
