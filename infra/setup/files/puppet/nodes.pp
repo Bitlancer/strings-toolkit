de602c885f559d87ae7c1af405258ab8a3e4d2f2
Matt Juszczak <mjuszczak@bitlancer.com>
2014-01-03 00:58:34 -0800

    removing files that are no longer (never were) used, also updating spacing in newdb.sh

--- infra/setup/files/puppet/nodes.pp
+++
@@ -1,6 +0,0 @@
-node default {
-  # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
-  # Configure the puppet master to use puppetdb
-  class { 'puppetdb::master::config': }
-}
--- openldap/newdb/newdb.sh
+++ openldap/newdb/newdb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+    echo "This script must be run as root" 1>&2
+    exit 1
 fi
 
 usage() { echo "Usage: $0 [-s <m|s>] [-d <domain>] [-t <tld>] [-i <increment>]" 1>&2; exit 1; }
